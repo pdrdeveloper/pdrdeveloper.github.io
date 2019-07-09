e3141bab36ae8599d48c3b16455c0701850dbb80
Pedro Henrique <pedrogamerlouco@outlook.com>
2019-07-09 17:42:27 -0300

    Added About Section and Scroll Animation - 0.5.5

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+$(document).ready(function($) {
+    $(".scroll").click(function(event){
+     event.preventDefault();
+     $('html,body').animate({scrollTop:$(this.hash).offset().top - 20}, 800);
+    });
+});
+
+$(document).ready(function($) {
+    $(".click_top").click(function(event){
+     event.preventDefault();
+     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+    });
+});
+
 function openNav() {
     document.getElementById('mobile_menu').style.width = "100%"
 }
 
 function closeNav() {
     document.getElementById('mobile_menu').style.width = "0"
+    document.getElementById('top_btn').style.display = 'flex'
 }
 
+function activateTopButton() {
+    document.getElementById('top_btn').style.display = 'flex'
+}
+
+function goToTopOfPage() {
+    document.getElementById('top_btn').style.display = 'none'
+}
