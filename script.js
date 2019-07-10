0fdd84ca7e3d384a55324ce6272479860b591e3c
Pedro Henrique <pedrogamerlouco@outlook.com>
2019-07-10 15:31:57 -0300

    Added Skills Section and Images - Version 0.6

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 $(document).ready(function($) {
     $(".scroll").click(function(event){
-     event.preventDefault();
-     $('html,body').animate({scrollTop:$(this.hash).offset().top - 20}, 800);
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top - 20}, 800);
     });
 });
 
 $(document).ready(function($) {
+    $(".scroll").click(function(event){
+        event.preventDefault();
+        $('.click_top').show();
+        $('.click_top').css("display", "flex")
+    });
+})
+
+$(document).ready(function($) {
     $(".click_top").click(function(event){
-     event.preventDefault();
-     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
     });
 });
 
@@ -18,7 +26,6 @@ function openNav() {
 
 function closeNav() {
     document.getElementById('mobile_menu').style.width = "0"
-    document.getElementById('top_btn').style.display = 'flex'
 }
 
 function activateTopButton() {
